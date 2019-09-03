3faf777d84275ad16f93590ceea896145b9e760d
Brian Park <brian.park@nih.gov>
2019-09-03 17:54:25 -0400

    add interpolateTicks

--- scale.js
+++ scale.js
@@ -5,13 +5,26 @@ export function getScale(inputRange, outputRange) {
   return value => outputMin + ratio * (value - inputMin) ;
 }
 
+// extends a range by the nearest power of 10 (eg: 0.2, 1.5 -> 0, 2)
+export function extendRange(range) {
+  let [r0, r1] = range;
+  return [r0, r1];
+}
+
 export function getTicks(min, max, numTicks) {
   numTicks = numTicks || 10;
 
-  let ticks = new Array(numTicks)
+  let ticks = new Array(numTicks);
   let interval = (max - min) / (numTicks - 1);
   for (let i = 0; i < numTicks; i ++)
       ticks[i] = min + i * interval;
 
   return ticks;
+}
+
+export function interpolateTicks(ticks) {
+  return ticks.map((value, index, array) => {
+    let previousValue = index > 0 ? array[index - 1] : 0;
+    return (value + previousValue) / 2;
+  });
 }
