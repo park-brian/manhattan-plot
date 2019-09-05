a566886c9f1c45bbe60eb1c467f2c373b29b55d5
Brian Park <brian.park@nih.gov>
2019-09-05 16:26:04 -0400

    plot variants on chromosome

--- script.js
+++ script.js
@@ -2,34 +2,35 @@ import { ManhattanPlot } from './manhattan-plot.js';
 import { systemFont } from './text.js';
 
 (async function main() {
-  let response = await fetch('summary.json').then(e => e.json());
+  let response = await fetch('variants.json').then(e => e.json());
   let ranges = await fetch('ranges.json').then(e => e.json());
   ranges = ranges.filter(e => e.chr <= 22); // filter out chromosomes 23+ for now
 
   let data = response.data;
   let columnIndexes = {
     chr: response.columns.indexOf('chr'),
-    bpAbs1Mb: response.columns.indexOf('bp_abs_1000kb'),
-    nLogP2: response.columns.indexOf('nlog_p2')
+    bp: response.columns.indexOf('bp'),
+    nLogP: response.columns.indexOf('nlog_p')
   };
   let withKeys = data => ({
     chr: data[columnIndexes.chr],
-    bpAbs1Mb: data[columnIndexes.bpAbs1Mb],
-    nLogP2: data[columnIndexes.nLogP2],
-  })
+    bp: data[columnIndexes.bp],
+    nLogP: data[columnIndexes.nlog_p],
+  });
 
   let config = {
     data: data,
     xAxis: {
-      title: [{text: `Ewing's Sarcoma`, font: `600 14px ${systemFont}`}],
-      key: columnIndexes.bpAbs1Mb,
-      ticks: ranges.map(r => r.max_bp_abs),
-      tickFormat: (tick, i) => ranges[i].chr,
-      labelsBetweenTicks: true,
-      allowSelection: true,
-      onSelected: (range, i) => {
-        console.log('selected x axis section', range, i)
-      }
+      title: [{text: `Ewing's Sarcoma - Chr 10`, font: `600 14px ${systemFont}`}],
+      key: columnIndexes.bp,
+      tickFormat: tick => (tick / 1e6).toFixed(3) + ' MB',
+      // ticks: ranges.map(r => r.max_bp_abs),
+      // tickFormat: (tick, i) => ranges[i].chr,
+      // labelsBetweenTicks: true,
+      // allowSelection: true,
+      // onSelected: (range, i) => {
+      //   console.log('selected x axis section', range, i)
+      // }
     },
     yAxis: {
       title: [
@@ -37,13 +38,14 @@ import { systemFont } from './text.js';
         {text: '10', textBaseline: 'middle', font: `600 10px ${systemFont}`},
         {text: `(p)`, font: `600 14px ${systemFont}`}
       ],
-      key: columnIndexes.nLogP2,
+      key: columnIndexes.nLogP,
       tickFormat: tick => (tick).toFixed(3),
     },
     point: {
       size: 4,
       opacity: 0.6,
-      color: (d, i) => d[columnIndexes.chr] % 2 ? '#005ea2' : '#e47833',
+      color: '#005ea2',
+      // color: (d, i) => d[columnIndexes.chr] % 2 ? '#005ea2' : '#e47833',
       tooltip: {
         trigger: 'click',
         class: 'custom-tooltip',
